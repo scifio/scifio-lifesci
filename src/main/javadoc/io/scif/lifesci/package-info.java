111dd7a9b994f3bf152b931aa055377f627c35e3
Mark Hiner <hinerm@gmail.com>
2015-01-22 02:36:49 -0600

    Update copyright for 2015

--- src/main/javadoc/io/scif/lifesci/package-info.java
+++ src/main/javadoc/io/scif/lifesci/package-info.java
@@ -2,10 +2,10 @@
  * #%L
  * SCIFIO Life Sciences Extension
  * %%
- * Copyright (C) 2013 - 2014 Open Microscopy Environment:
- *   - Board of Regents of the University of Wisconsin-Madison
- *   - Glencoe Software, Inc.
- *   - University of Dundee
+ * Copyright (C) 2013 - 2015 Open Microscopy Environment:
+ * 	- Board of Regents of the University of Wisconsin-Madison
+ * 	- Glencoe Software, Inc.
+ * 	- University of Dundee
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
